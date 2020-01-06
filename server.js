3ae859497161a15661df007eb174e52c972f05ba
Joscandy Nunez <50030017+jcandyn@users.noreply.github.com>
2020-01-06 09:10:33 -0500

    files uploaded again but with sequel

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
-// Dependencies
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
 // =============================================================
 var express = require("express");
-var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
-var router = require('./controllers/aussi_controller.js');
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 5000;
+var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(methodOverride('_method'));
 
-
-// Star Wars Characters (DATA)
-// =============================================================
-
-// app.get("/test", function(req, res) {
-//     // res.send("Welcome to the Star Wars Page!")
-//     res.send("test!")
-//     console.log("connected")
-//   });
-
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-
-app.use('/', router);
+// Static directory
 app.use(express.static("public"));
 
-  var port = process.env.PORT || 5000;
-app.listen(PORT, function() {
-  console.log("App is listening on port " + port);
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
